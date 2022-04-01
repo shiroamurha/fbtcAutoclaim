ee9f9a7e9440747bf824d6acb1a034538bb3852c
Shiro Amurha <46139101+shiroamurha@users.noreply.github.com>
2022-04-01 14:50:22 -0300

    Add files via upload

--- autoclaimer.py
+++ autoclaimer.py
@@ -1,4 +1,5 @@
 from playwright.sync_api import sync_playwright
+from time import sleep
 import json
 
 
@@ -7,14 +8,20 @@ class Autoclaimer():
 
     def __init__(self):
         
-        self.start_driver()
-        self.claim()
+        # main worker loop, every 1 hour (3600sec) does the three methods in a row 
+        while True:
+            
+            self.start_driver()
+            self.claim()
+            self.close_driver()
+            
+            sleep(3600)
 
     def start_driver(self):
         
         # page and session (context) initiation 
-        playwright = sync_playwright().start() 
-        self.browser = playwright.webkit.launch()
+        self.playwright = sync_playwright().start() 
+        self.browser = self.playwright.webkit.launch()
         context = self.browser.new_context()
         self.page = context.new_page()
 
@@ -30,31 +37,48 @@ class Autoclaimer():
                 )
             )
         )
+        
+        # after setting cookies, refreshes the page
         self.page.goto("https://freebitco.in/?op=home")
 
     ##
 
     def claim(self):
         
+        # tries to roll without captcha 
         try:
+            
+            # sends directly the event of button's onclick 
             play_without_captcha = self.page.dispatch_event(
             
                 'div#play_without_captchas_button',
                 'click'
-            
+  
             )
             
+            # roll click
             roll_button = self.page.locator(
                 
                 'input[id="free_play_form_button"]'
                 
             ).click()
             
+        # if any exception, breaks the code and then shows line with exception
         except Exception as e:
             print(e, ' not able to detect button')
         
         else:
-            print(play_without_captcha, 'clicked!')
+            print(roll_button, 'clicked!')
+    
+    def close_driver(self):
+        
+        #closes browser and driver
+        self.browser.close()
+        self.playwright.stop()
+        
+        
             
 if __name__ == '__main__':
+    
+    # if code is running by its side, then calls autoclaimer class
     Autoclaimer()
